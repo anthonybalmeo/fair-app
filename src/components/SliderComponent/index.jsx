4b358185e14f45ac1d2e8965e16a450339a2de0c
Anthony Balmeo <anthonybalmeo@Anthonys-iMac.local>
2018-12-18 03:23:41 -0800

    slider component wip

--- src/components/SliderComponent/index.jsx
+++ src/components/SliderComponent/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import 'rc-slider/assets/index.css';
-import Slider, { Range } from 'rc-slider';
+import Slider from 'react-rangeslider'
+import '../../../styles/components/SliderComponent/index.sass'
 
 class SliderComponent extends React.Component {
   constructor(props) {
@@ -8,36 +8,29 @@ class SliderComponent extends React.Component {
     this.state = {
       minimum: props.min,
       maximum: props.max,
+      budget: props.max,
     };
   }
-  onSliderChange = (value) => {
-    console.log(value);
-  }
-  onMinChange = (e) => {
+  handleOnChange = (value) => {
     this.setState({
-      min: +e.target.value || this.props.min,
-    });
+      budget: value
+    })
   }
-  onMaxChange = (e) => {
-    this.setState({
-      max: +e.target.value || this.props.max,
-    });
-  }
+
  render () {
-   const { min, max } = this.props
+   const { minimum, maximum, budget } = this.state
+   const sliderLabel = {
+     [minimum]: `${minimum}`,
+     [maximum]: `${maximum}`,
+   }
   return (
-    <div>
-      <label>Min: </label>
-      <input type="number" value={this.state.minimum} onChange={this.onMinChange} />
-      <br />
-      <label>Max: </label>
-      <input type="number" value={this.state.maximum} onChange={this.onMaxChange} />
-      <br /><br />
-      <Slider defaultValue={50}
+    <div className='fair-slider'>
+      <Slider
         min={this.state.minimum}
         max={this.state.maximum}
-        onChange={this.onSliderChange}
-        // marks={{min, max}}
+        value={this.state.budget}
+        onChange={this.handleOnChange}
+        labels={sliderLabel}
       />
     </div>
   )
