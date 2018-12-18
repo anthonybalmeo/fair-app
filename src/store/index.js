334c8eb0845739cd6e6ff300379c7a3bb4eefa76
Anthony Balmeo <anthonybalmeo@Anthonys-iMac.local>
2018-12-18 03:23:18 -0800

    Update store issue

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   rootReducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(thunk))
   );
